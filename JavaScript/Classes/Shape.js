830cd3c69c31bfca9ca7442599815104e1cf2977
madhuredra <madhuredra.tiwari@zemosolabs.com>
2023-09-24 02:23:27 +0530

    fixed styling

--- JavaScript/Classes/Shape.js
+++ JavaScript/Classes/Shape.js
@@ -1,13 +1,13 @@
 /**
  * @author dev-madhurendra
- * 
- * 
- * @class : In Object-Oriented Programming (OOP), a class is a blueprint or template 
- * for creating objects (instances). It defines the properties (also known as 
- * attributes or fields) and methods (functions) that the objects created from 
- * the class will have. JavaScript, starting from ECMAScript 6 (ES6), introduced 
+ *
+ *
+ * @class : In Object-Oriented Programming (OOP), a class is a blueprint or template
+ * for creating objects (instances). It defines the properties (also known as
+ * attributes or fields) and methods (functions) that the objects created from
+ * the class will have. JavaScript, starting from ECMAScript 6 (ES6), introduced
  * support for classes, making it easier to work with OOP principles in JavaScript.
- * 
+ *
  * Represents a basic shape.
  * @class
  */
@@ -17,25 +17,25 @@ class Shape {
    * @param {number} x - The X coordinate of the shape's position.
    * @param {number} y - The Y coordinate of the shape's position.
    */
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
+  constructor (x, y) {
+    this.x = x
+    this.y = y
   }
 
   /**
    * Get the X coordinate of the shape.
    * @returns {number} The X coordinate.
    */
-  getX() {
-    return this.x;
+  getX () {
+    return this.x
   }
 
   /**
    * Get the Y coordinate of the shape.
    * @returns {number} The Y coordinate.
    */
-  getY() {
-    return this.y;
+  getY () {
+    return this.y
   }
 
   /**
@@ -43,11 +43,10 @@ class Shape {
    * @param {number} newX - The new X coordinate.
    * @param {number} newY - The new Y coordinate.
    */
-  move(newX, newY) {
-    this.x = newX;
-    this.y = newY;
+  move (newX, newY) {
+    this.x = newX
+    this.y = newY
   }
 }
 
 export default Shape
-
